f1bd21af5871dfc6a23a8583fde3d51d24f6b7ef
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2020-07-11 19:22:47 +0500

    added database and notes directory

---
+++ app/src/main/java/com/thefuturestic/notesapp/Note.kt
@@ -0,0 +1,15 @@
+package com.thefuturestic.notesapp
+
+class Note{
+
+    var nodeID:Int?=null
+    var nodeName:String?=null
+    var nodeDes:String?=null
+
+    constructor(nodeID:Int,nodeName:String,nodeDes:String){
+        this.nodeID=nodeID
+        this.nodeName=nodeName
+        this.nodeDes=nodeDes
+    }
+
+}
